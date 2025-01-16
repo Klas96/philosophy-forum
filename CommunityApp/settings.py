e4c4b5a2542cd8c3e0cc8d3ef1c379a3e24e3839
Klas Holmgren <klas0holmgren@gmail.com>
2025-01-16 11:26:02 +0100

    make it better

--- CommunityApp/settings.py
+++ CommunityApp/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -191,3 +192,5 @@ BATON = {
         },
     )
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
